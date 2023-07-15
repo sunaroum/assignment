9952bc5b823a70120183538c78f846adf3c2cbfb
KJK <137695045+normalKimJk@users.noreply.github.com>
2023-07-15 15:42:36 +0900

    Delete 김주경 directory

--- 김주경/수나로움 1주차 HOMEWORK (1-1).py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-score = np.array([[70, 45, 60], [30, 25, 95], [23, 53, 100]])
-
-df = pd.DataFrame(score, columns=['Math', 'Chemistry', 'Physics'])
-
-print(df)
